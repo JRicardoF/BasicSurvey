f8e08a5c2098027c585ce9d4b3b6f4646caa5b16
JRicardoF <ricardosfj1@hotmail.com>
2021-04-06 08:37:35 -0700

    Add project files.

---
+++ BasicSurvey/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BasicSurvey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your Name?");
+            var Name = Console.ReadLine();
+
+            Console.WriteLine("How old are you? (Just insert the number please)");
+            var Age = Console.ReadLine();
+
+            Console.WriteLine("What month were you born in?");
+            var MonthBirth = Console.ReadLine();
+
+            Console.WriteLine("Your Name is "+ Name + " and you have "+ Age + " years old and you born in " + MonthBirth);
+        }
+    }
+}
